857d89a803da46b1cb42c2c2e779a3e1420430f0
sgnu <43324299+sgnu@users.noreply.github.com>
2020-05-11 18:44:38 -0400

    Add a few more phrases

--- src/TypingTests.js
+++ src/TypingTests.js
@@ -1,6 +1,8 @@
 const TypingTests = [
-	"The quick brown fox jumps over the lazy brown dog.",
-	"Its lit dog."
+	"The quick brown fox jumps over the lazy dog",
+	"Quick zephyrs blow, vexing daft Jim",
+	"Sphinx of black quartz, judge my vow",
+	"The job requires extra pluck and zeal from every young wage earner"
 ]
 
 export default TypingTests;
