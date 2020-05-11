c8fdebab2d7d7029a0138574514684bc0817cdbc
sgnu <43324299+sgnu@users.noreply.github.com>
2020-05-11 18:54:41 -0400

    Updates

---
+++ precache-manifest.a67cf85516c4e51bcf268fcb19ea7305.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d01e68cc16f029d0ae9eba899e9ee7e5",
+    "url": "/type-tester/index.html"
+  },
+  {
+    "revision": "f605987e88794b8905f5",
+    "url": "/type-tester/static/css/main.adc428fe.chunk.css"
+  },
+  {
+    "revision": "b9a46eb72f49fc4747eb",
+    "url": "/type-tester/static/js/2.c005656f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/type-tester/static/js/2.c005656f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f605987e88794b8905f5",
+    "url": "/type-tester/static/js/main.d0c71e33.chunk.js"
+  },
+  {
+    "revision": "b392cfb2b45b4049b8a9",
+    "url": "/type-tester/static/js/runtime-main.fc702d67.js"
+  }
+]);
--- src/TypingTests.js
+++
@@ -1,8 +0,0 @@
-const TypingTests = [
-	"The quick brown fox jumps over the lazy dog",
-	"Quick zephyrs blow, vexing daft Jim",
-	"Sphinx of black quartz, judge my vow",
-	"The job requires extra pluck and zeal from every young wage earner"
-]
-
-export default TypingTests;
